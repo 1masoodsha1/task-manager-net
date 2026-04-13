bbcae6aa73c0b5c39b1814a599340afe97997d1c
Muhammad Masood <1masoodsha1@gmail.com>
2026-04-13 15:24:24 -0400

    Remove unused logic

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,4 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+// This project uses standalone Angular bootstrapping from src/main.ts.
+// Do not declare standalone components in an NgModule here.
 
-import { AppComponent } from './app.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
-
-@NgModule({
-declarations: [
-AppComponent,
-TaskFormComponent,
-TaskListComponent
-],
-imports: [
-BrowserModule,
-HttpClientModule,
-ReactiveFormsModule
-],
-providers: [],
-bootstrap: [AppComponent]
-})
-export class AppModule { }
+export {}
